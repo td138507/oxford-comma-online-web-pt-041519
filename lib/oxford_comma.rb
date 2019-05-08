1c9daaa16d11697807ddb28bcfdc826687e55d29
Taylor Doyle <tcd56@cornell.edu>
2019-05-08 19:21:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-  array = ["fiddleheads","okra","kohlrabi"]
-  array.join(", ")
-elsif array.length > 2
-  array << " and "
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and")
+  end
+  array.join(",")
+end
 end

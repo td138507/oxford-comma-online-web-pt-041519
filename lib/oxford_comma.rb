4d4b058a810e361800320a14bffa1f34c8be6e41
Taylor Doyle <tcd56@cornell.edu>
2019-05-08 19:26:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
 
-end
+  if array.length == 1
+    array [0]
+  elsif array.length == 2
+    array [0]+" and "+array[1]
+  else
+    array [-1] = "and "+array[-1]
+    array.join(", ")
+  end
+  end
